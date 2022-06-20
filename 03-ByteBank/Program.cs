6643d98b751bb0264123edf15ec8bbb6e884eea6
Mateus Prado <mateusvilarinodoprado@outlook.com>
2022-06-20 02:11:56 -0300

    Adicionando Arquivos

---
+++ 03-ByteBank/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaGabriela = new ContaCorrente();
+
+            contaGabriela.titular = "Gabriela";
+            contaGabriela.agencia = 863;
+            contaGabriela.numeroConta = 863452;
+
+            ContaCorrente contaGabrielaCosta = new ContaCorrente();
+
+            contaGabrielaCosta.titular = "Gabriela";
+            contaGabrielaCosta.agencia = 863;
+            contaGabrielaCosta.numeroConta = 863452;
+
+            Console.WriteLine("Igualdade de tipo de referência, Número conta: " + (contaGabriela == contaGabrielaCosta));
+
+            int idade = 27;
+            int idadeMaisUmaVez = 27;
+
+            Console.WriteLine("Igualdade de tipo de valor, Idade: " + (idade == idadeMaisUmaVez));
+
+            contaGabriela = contaGabrielaCosta;
+
+            Console.WriteLine("Igualdade de tipo de referência COM um objeto recebendo o valor(ENDEREÇO) de outro, Número conta:  " + 
+                (contaGabriela == contaGabrielaCosta));
+
+            contaGabriela.saldo = 300;
+
+            Console.WriteLine("Conta Gabriela: " + contaGabriela.saldo);
+            Console.WriteLine("Conta Gabriela Costa: " + contaGabrielaCosta.saldo);
+
+            Console.ReadLine();
+
+            if (contaGabriela.saldo >= 100 && contaGabriela.saldo>0)
+            {
+                contaGabriela.saldo -= 100;
+            }
+        }
+    }
+}
---
+++ 05-ByteBank/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cliente gabriela = new Cliente();
+
+            //gabriela.nome = "Gabriela";
+            //gabriela.profissao = "Desenvolvedora C#";
+            //gabriela.cpf = "021.045.110-62";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            //conta.titular = gabriela;
+
+            conta.titular = new Cliente();
+            conta.titular.nome = "Gabriela Costa";
+            conta.titular.cpf = "021.045.110-62";
+            conta.titular.profissao = "Desenvolvedora C#";
+
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numeroConta = 5634527;
+
+            //Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+            Console.WriteLine(conta.titular.cpf);
+            Console.WriteLine(conta.titular.profissao);
+
+            Console.ReadLine();
+        }
+    }
+}
